bca3a3305e18063762b2b5e9ef6de1b6ecc432a8
jaejun1130 <wjswowns1234@gmail.com>
2023-09-10 21:20:38 +0900

    Concurrency

---
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -0,0 +1,65 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.facade.OptimisticLockStockFacade;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StockServiceTest {
+
+    @Autowired
+    private OptimisticLockStockFacade optimisticLockStockFacade;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void insert() {
+        Stock stock = new Stock(1L, 100L);
+
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @AfterEach
+    public void delete() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void 동시에_요청_낙관적_락() throws InterruptedException {
+        int threadCount = 2;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    optimisticLockStockFacade.decrease(1L, 3L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (6) = 94
+        assertEquals(94, stock.getQuantity());
+    }
+}
